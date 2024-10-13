d9c8b0059d5c067764848efeb15c199ee364543d
Braynha-10 <bryansscarvalho16@gmail.com>
2024-10-13 19:16:05 -0300

    version 1.1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Fastfy é um framework de APIs
-# Minimal API é um conceito de uma API mais que possui uma arquitetura minimalista e simples geralmente para listagem, prover informaçoes etc.
-# TypeScript - Javaascript tipado
-# 2 endpoints - pilotos e equipes atuais
-# boilerplate é um projeto base que ja vem com um escopo inicial pronto facilitando o inicio de projetos que necessitam uma configuração inicial antecipada
+### Fastfy é um framework de APIs
+### Minimal API é um conceito de uma API mais que possui uma arquitetura minimalista e simples geralmente para listagem, prover informaçoes etc.
+### TypeScript - Javascript tipado
+### 2 endpoints - pilotos e equipes atuais
+### boilerplate é um projeto base que ja vem com um escopo inicial pronto facilitando o inicio de projetos que necessitam uma configuração inicial antecipada
+### CORS - proteção de compartilhamento de recursos entre origens, controle de quem está chamando a API e quem tem acesso aos recursos.
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
+import cors from "@fastify/cors"
 
 const server = fastify({logger: true});
+
+server.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST"]
+});
 
 const teams = [
     {
@@ -17,8 +23,39 @@ const teams = [
         id: 3,
         name: "Red Bull Racing",
         base: "Milton Keynes, United Kingdom"
+    },
+    {
+        id: 4,
+        name: "Alpine",
+        base: "Enstone, United Kingdom"
+    },
+    {
+        id: 5,
+        name: "Aston Martin",
+        base: "Silverstone, United Kingdom"
+    },
+    {
+        id: 6,
+        name: "Ferrari",
+        base: "Maranello, Italy"
+    },
+    {
+        id: 7,
+        name: "Haas",
+        base: "Kannapolis, United States"
+    },
+    {
+        id: 8,
+        name: "AlphaTauri",
+        base: "Faenza, Italy"
+    },
+    {
+        id: 9,
+        name: "Williams",
+        base: "Grove, United Kingdom"
     }
 ];
+
 
 const pilots = [
     {
@@ -35,8 +72,89 @@ const pilots = [
         id: 3,
         name: "Felipe Massa",
         base: "Ferrari"
+    },
+    {
+        id: 4,
+        name: "Norris",
+        base: "McLaren"
+    },
+    {
+        id: 5,
+        name: "Leclerc",
+        base: "Ferrari"
+    },
+    {
+        id: 6,
+        name: "Sainz",
+        base: "Ferrari"
+    },
+    {
+        id: 7,
+        name: "Russell",
+        base: "Mercedes"
+    },
+    {
+        id: 8,
+        name: "Perez",
+        base: "Red Bull Racing"
+    },
+    {
+        id: 9,
+        name: "Alonso",
+        base: "Alpine"
+    },
+    {
+        id: 10,
+        name: "Piastri",
+        base: "McLaren"
+    },
+    {
+        id: 11,
+        name: "Ocon",
+        base: "Alpine"
+    },
+    {
+        id: 12,
+        name: "Gasly",
+        base: "AlphaTauri"
+    },
+    {
+        id: 13,
+        name: "Ricciardo",
+        base: "McLaren"
+    },
+    {
+        id: 14,
+        name: "Stroll",
+        base: "Aston Martin"
+    },
+    {
+        id: 15,
+        name: "Hulkenberg",
+        base: "Haas"
+    },
+    {
+        id: 16,
+        name: "Sargeant",
+        base: "Williams"
+    },
+    {
+        id: 17,
+        name: "Albon",
+        base: "Williams"
+    },
+    {
+        id: 18,
+        name: "Magnussen",
+        base: "Haas"
+    },
+    {
+        id: 19,
+        name: "Tsunoda",
+        base: "AlphaTauri"
     }
 ];
+
 
 server.get("/teams", async(req, res)=>{
     res.type("application/json").code(200)
@@ -48,6 +166,23 @@ server.get("/pilots", async(req, res)=>{
     res.type("application/json").code(200)
 
     return { pilots };
+});
+
+interface RouteParams{
+    id: string
+}
+
+server.get<{Params: RouteParams}>("/pilots/:id", async(req, res)=>{
+    const id = parseInt(req.params.id);
+    const pilot = pilots.find((p) => p.id === id);
+
+    if(!pilot){
+        res.type("application/json").code(404);
+        return { message: "Pilot Not Found" }
+    }else{
+        res.type("application/json").code(200);
+        return { pilot };
+    }
 });
 
 server.listen({port: 3333}, ()=>{
